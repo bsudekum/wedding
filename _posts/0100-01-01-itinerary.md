6982a435ca8681ddf8624c4bcf8d338b9e248a5f
rsudekum <rsudekum@gmail.com>
2013-11-24 18:49:49 -0500

    Updates

---
+++ _posts/0100-01-01-itinerary.md
@@ -0,0 +1,23 @@
+---
+published: true
+layout: default
+title: Itinerary
+---
+<div class='col3 margin1'>
+  <h1>Friday, July 11</h1>
+  <h3>Let’s Do Drinks!</h3>
+  <h3>???pm at Bar Toma</h3>
+  <h3><a href='https://www.google.com/maps/preview#!q=110+E+Pearson+St.&data=!1m4!1m3!1d3128!2d-87.6251533!3d41.8976691!4m12!2m11!1m10!1s0x880fd353a74fd68d%3A0xa0bc44c68ffa9730!3m8!1m3!1d12417!2d-77.0387255!3d38.9198195!3m2!1i1024!2i768!4f13.1' target='_blank'>110 E Pearson St.</a></h3>
+</div>
+
+<div class='col3 margin1'>
+  <h1>Saturday, July 12</h1>
+  <h3>We Get Married</h3>
+  <h3>6pm at Prairie Production</h3>
+  <h3><a href='https://www.google.com/maps/preview#!q=1314+W+Randolph+St.+chicago&data=!4m12!2m11!1m10!1s0x880e2d27bfa7fabb%3A0xfc273312bdae1567!3m8!1m3!1d12417!2d-77.0387255!3d38.9198195!3m2!1i1024!2i768!4f13.1' targer='_blank'>1314 W Randolph St.</a></h3>
+</div>
+
+<div class='col3 margin1'>
+  <h1>Sunday, July 13</h1>
+  <h3>No Plans</h3>
+</div>
--- site.js
+++ site.js
@@ -6,47 +6,53 @@
 if(window.location.pathname == '/update/'){
 	twitterFetcher.fetch('379266648101765120', 'twitter', 100, true, false, false, close);
 	function close(){
-		setTimeout(function(){
-			$('.close').click(function(){
-				$(this).parent('li').fadeOut();
-			});
-		},1000);
 	}
 }else{
 	twitterFetcher.fetch('379266648101765120', 'twitter', 1, true, false, false, addUpdate);
 	function addUpdate(){
-		setTimeout(function(){
-			$('#twitter p').prepend('<strong class="quiet">Update: </strong>')
-			$('.close').click(function(){
-				$(this).parent('li').fadeOut();
-			});
-		},1000)
 	}
 }
 
-var map = L.mapbox.map('map', 'examples.map-9ijuk24y')
-	.setView([41.88456316303698,-87.65976190567017], 17);
+if(window.location.pathname == '/places/'){
+	var map = L.mapbox.map('map', 'examples.map-9ijuk24y')
+		.setView([41.88456316303698,-87.65976190567017], 17);
 
-var weddingMarker = L.marker([41.88456316303698,-87.65976190567017], {
-        icon: L.mapbox.marker.icon({
-            'marker-symbol': 'star-stroked',
-            'marker-color': '77B66F'
-        })
-    })
-	.bindPopup('<a href="#" class="center">Wedding</a>')
-	.addTo(map)
-	.openPopup();
+	var weddingMarker = L.marker([41.88456316303698,-87.65976190567017], {
+	        icon: L.mapbox.marker.icon({
+	            'marker-symbol': 'star-stroked',
+	            'marker-color': '77B66F'
+	        })
+	    })
+		.bindPopup('<a href="#" class="center">Wedding</a>')
+		.addTo(map)
+		.openPopup();
 
-var sudekumMarker = L.marker([42.11401459274903,-87.73608952760695])
-	.bindPopup('<a href="#" class="center">The Sudekum\'s House</a>')
-	.addTo(map)
+	var sudekumMarker = L.marker([42.11401459274903,-87.73608952760695])
+		.bindPopup('<a href="#" class="center">The Sudekum\'s House</a>')
+		.addTo(map)
 
-$('.place').click(function(){
-    var pos = $(this).attr('data-position');
-    
-    if (pos) {
-        var loc = pos.split(',');
-        map.setView(loc, 16);
-    }
+	$('.place').click(function(){
+	    var pos = $(this).attr('data-position');
+	    
+	    if (pos) {
+	        var loc = pos.split(',');
+	        map.setView(loc, 16);
+	    }
+	});
+}else{
+	console.log('notplaces')
+}
 
+$('.content-block img').click(function(){
+	var path = $(this).attr('src');
+	$('.zoom-img img').attr('src', path);
+	$('.zoom-img img').fadeIn(function(){
+		$('.black').fadeIn();	
+	});
+	
+	$('.black').click(function(){
+		$('.zoom-img').fadeOut();
+		$('.zoom-img img').attr('src','')
+		$('.black').fadeOut();
+	})
 });
